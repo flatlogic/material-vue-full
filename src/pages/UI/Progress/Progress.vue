ba1342bbe1acb3a825b6d9cf8bf37b0512873245
Blari <Blarior@gmail.com>
2020-08-14 10:30:53 +0300

    Finish progress page

--- src/pages/UI/Progress/Progress.vue
+++ src/pages/UI/Progress/Progress.vue
@@ -34,7 +34,7 @@
             <p class="fs-normal card-dark-grey mb-5">Using the <code>indeterminate</code> prop, <code>v-progress-linear</code> continuously animates.</p>
             <template v-for="prog in progress">
               <v-progress-linear
-                v-bind:key="prog.value"
+                :key="prog.value"
                 v-model="prog.value"
                 :color="prog.color"
               ></v-progress-linear>
@@ -155,7 +155,7 @@
               that there is some action taking place. You can use any combination of <code>buffer-value</code> and <code>value</code> to achieve your design.</p>
             <template v-for="prog in progress">
               <v-progress-linear
-                v-bind:key="prog.color"
+                :key="prog.color"
                 :value="prog.value"
                 striped
                 height="20"
@@ -292,11 +292,10 @@
           </v-card-text>
         </v-card>
       </v-col>
-
       <v-col lg=6 cols=12>
         <v-card class="overflow-hidden mx-1 mb-1">
           <v-card-title class="pa-5 pb-3">
-            <p>Striped</p>
+            <p>Rotate</p>
             <v-spacer></v-spacer>
             <v-menu>
               <template v-slot:activator="{ on, attrs }">
@@ -320,9 +319,19 @@
             </v-menu>
           </v-card-title>
           <v-card-text class="pa-5 pt-0">
-            <p class="fs-normal card-dark-grey mb-5">The <code>stream</code> property works with buffer-value to convey to the user
-              that there is some action taking place. You can use any combination of <code>buffer-value</code> and <code>value</code> to achieve your design.</p>
-
+            <p class="fs-normal card-dark-grey mb-5">The <code>rotate</code> prop gives you the ability to customize the <code>v-progress-circular</code>'s origin.</p>
+            <div class="d-flex justify-space-around align-center">
+              <template v-for="circle in progressCircleRotate">
+                <v-progress-circular
+                    :value="value"
+                    width="15"
+                    size="100"
+                    :rotate="circle.rotate"
+                    :key="circle.id"
+                    :color="circle.color"
+                >{{ value }}</v-progress-circular>
+              </template>
+            </div>
           </v-card-text>
         </v-card>
       </v-col>
@@ -332,68 +341,110 @@
 
 <script>
 
-    export default {
-        name: 'Progress',
-        data () {
-            return {
-                menu: [
-                    'Edit',
-                    'Copy',
-                    'Delete',
-                    'Print'
-                ],
-                progress: [
-                    {
-                        color: 'primary',
-                        value: 15,
-                    },
-                    {
-                        color: 'warning',
-                        value: 30,
-                    },
-                    {
-                        color: 'secondary',
-                        value: 45
-                    },
-                    {
-                        color: 'success',
-                        value: 60
-                    },
-                ],
-                progressCircle: [
-                    {
-                        color: 'primary',
-                        value: 15,
-                        size: 100,
-                        width: 15
-                    },
-                    {
-                        color: 'warning',
-                        value: 30,
-                        size: 80,
-                        width: 10
-                    },
-                    {
-                        color: 'secondary',
-                        value: 45,
-                        size: 60,
-                        width: 5
-                    },
-                    {
-                        color: 'success',
-                        value: 60,
-                        size: 40,
-                        width: 3
-                    },
-                    {
-                        color: 'info',
-                        value: 75,
-                        size: 20,
-                        width: 2
-                    },
-                ],
-            }
-        }
-    }
+  export default {
+    name: 'Progress',
+    data () {
+      return {
+        menu: [
+          'Edit',
+          'Copy',
+          'Delete',
+          'Print'
+        ],
+        progress: [
+          {
+              color: 'primary',
+              value: 15,
+          },
+          {
+              color: 'warning',
+              value: 30,
+          },
+          {
+              color: 'secondary',
+              value: 45
+          },
+          {
+              color: 'success',
+              value: 60
+          },
+        ],
+        progressCircle: [
+          {
+              color: 'primary',
+              value: 15,
+              size: 100,
+              width: 15
+          },
+          {
+              color: 'warning',
+              value: 30,
+              size: 80,
+              width: 10
+          },
+          {
+              color: 'secondary',
+              value: 45,
+              size: 60,
+              width: 5
+          },
+          {
+              color: 'success',
+              value: 60,
+              size: 40,
+              width: 3
+          },
+          {
+              color: 'info',
+              value: 75,
+              size: 20,
+              width: 2
+          },
+        ],
+        progressCircleRotate: [
+          {
+            id: 1,
+            color: 'primary',
+            value: this.value,
+            rotate: 360
+          },
+          {
+            id: 2,
+            color: 'warning',
+            value: this.value,
+            rotate: -90
+          },
+          {
+            id: 3,
+            color: 'secondary',
+            value: this.value,
+            rotate: 90
+          },
+          {
+            id: 4,
+            color: 'success',
+            value: this.value,
+            rotate: 360
+          },
+          {
+            id: 5,
+            color: 'info',
+            value: this.value,
+            rotate: 180
+          },
+        ],
+        value: 0,
+        interval: {}
+      }
+    },
+    mounted () {
+      this.interval = setInterval(() => {
+        this.value === 100 ? this.value = 0 : this.value += 5}, 1000)
+    },
+
+    beforeDestroy () {
+      clearInterval(this.interval)
+    },
+  }
 
 </script>
