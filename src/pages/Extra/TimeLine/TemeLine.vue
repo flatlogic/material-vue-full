721c1d29b7081e03c68ceadf3cb427f536a1d7f4
Blari <Blarior@gmail.com>
2020-08-21 11:19:32 +0300

    Finish time line

--- src/pages/Extra/TimeLine/TemeLine.vue
+++ src/pages/Extra/TimeLine/TemeLine.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container fluid class="timeline-title-page">
     <v-row no-gutters class="d-flex mt-10 mb-6">
-      <h1 class="timeline-title">Time Line</h1>
+      <h1 class="page-title">Time Line</h1>
     </v-row>
     <v-timeline reverse align-top :dense="$vuetify.breakpoint.smAndDown">
       <v-timeline-item
@@ -11,12 +11,11 @@
         :icon="item.icon"
       ><span v-html="item.opposite" slot="opposite"></span>
         <v-card color="white">
-          <v-card-title>
-            <v-list-item inactive>
+          <v-card-title class="pb-0">
+            <v-list-item inactive class="pl-0">
               <v-list-item-avatar>
                 <v-img src="https://cdn.vuetifyjs.com/images/lists/1.jpg"></v-img>
               </v-list-item-avatar>
-
               <v-list-item-content>
                 <v-list-item-title v-html="item.title"></v-list-item-title>
                 <v-list-item-subtitle v-html="item.subtitle"></v-list-item-subtitle>
@@ -24,6 +23,7 @@
             </v-list-item>
           </v-card-title>
           <v-card-text class="white text--primary px-0">
+            <p class="mx-5 mb-0"> {{ item.content.text }} </p>
             <GmapMap v-if="item.content.googleMap"
               :center="{lat: -37.813179, lng: 144.950259}"
               :zoom="12"
@@ -33,23 +33,53 @@
                   :position="{lat: -37.813179, lng: 144.950259}"
               />
             </GmapMap>
+            <v-img v-else-if="item.content.image" src="@/assets/img/time-line/mountains.jpeg"></v-img>
           </v-card-text>
-          <v-card-text class="white text--primary">
-            <v-btn icon class="mr-3">
-              <v-icon>mdi-heart</v-icon>
+          <v-card-text class="white text--primary py-0">
+            <v-btn icon class="mr-3" >
+              <v-icon :color="'rgba(110, 110, 110, 0.6)'">mdi-heart</v-icon>
             </v-btn>
-            <v-btn icon>
-              <v-icon>mdi-message-text</v-icon>
-            </v-btn>
-            <p>Lorem ipsum dolor sit amet, no nam oblique veritus. Commune scaevola imperdiet nec ut, sed euismod convenire principes at. Est et nobis iisque percipit, an vim zril disputando voluptatibus, vix an salutandi sententiae.</p>
-            <v-btn
-              :color="item.color"
-              class="mx-0"
-              outlined
-            >
-              Button
+            <v-btn icon >
+              <v-icon :color="'rgba(110, 110, 110, 0.6)'">mdi-message-text</v-icon>
             </v-btn>
           </v-card-text>
+          <v-list two-line>
+            <template v-for="(item, index) in item.comments">
+
+              <v-divider
+                  v-if="item.divider"
+                  :key="index"
+              ></v-divider>
+              <v-list-item
+                  :key="item.title"
+              >
+                <v-list-item-avatar>
+                  <v-img :src="item.avatar"></v-img>
+                </v-list-item-avatar>
+
+                <v-list-item-content>
+                  <v-list-item-title v-html="item.title"></v-list-item-title>
+                  <v-list-item-subtitle v-html="item.subtitle"></v-list-item-subtitle>
+                </v-list-item-content>
+              </v-list-item>
+
+            </template>
+            <v-divider></v-divider>
+            <v-list-item class="mt-2">
+              <v-btn
+                fab
+                small
+                color="primary"
+                elevation="0"
+                class="mr-3"
+              >P</v-btn>
+              <v-text-field
+                dense
+                label="Write your comment..."
+              ></v-text-field>
+            </v-list-item>
+
+          </v-list>
         </v-card>
       </v-timeline-item>
     </v-timeline>
@@ -57,52 +87,14 @@
 </template>
 
 <script>
+import mock from "@/pages/Extra/TimeLine/mock";
 export default {
   name: 'TimeLine',
   data: () => ({
-    items: [
-      {
-        color: 'secondary',
-        icon: 'mdi-navigation',
-        opposite: 'yesterday<h5>8:03 pm</h5>',
-        title: 'Jessica Nilson @jess',
-        subtitle: '10:12 am - Publicly near Minsk',
-        content: {
-          googleMap: true
-        }
-      },
-      {
-        color: 'info',
-        icon: 'mdi-message',
-        opposite: 'yesterday<h5>8:03 pm</h5>',
-        title: 'Jessica Nilson @jess',
-        subtitle: '10:12 am - Publicly near Minsk',
-        content: ''
-      },
-      {
-        color: 'green lighten-1',
-        icon: 'mdi-coffee',
-        opposite: 'yesterday<h5>8:03 pm</h5>',
-        title: 'Jessica Nilson @jess',
-        subtitle: '10:12 am - Publicly near Minsk',
-        content: ''
-      },
-      {
-        color: 'indigo',
-        icon: 'mdi-alpha-j',
-        opposite: 'yesterday<h5>8:03 pm</h5>',
-        title: 'Jessica Nilson @jess',
-        subtitle: '10:12 am - Publicly near Minsk',
-        content: ''
-      },
-    ],
+    mock,
+    items: mock.items,
   }),
 }
 </script>
 
-<style>
-.timeline-title-page .v-timeline-item__opposite {
-    align-self: self-start;
-}
-
-</style>
+<style src="./Timeline.scss" lang="scss"/>
---
+++ src/pages/Extra/TimeLine/mock.js
@@ -0,0 +1,77 @@
+export default {
+    items: [
+        {
+            color: 'secondary',
+            icon: 'mdi-navigation',
+            opposite: 'yesterday<h5>8:03 pm</h5>',
+            title: 'Jessica Nilson @jess',
+            subtitle: '10:12 am - Publicly near Minsk',
+            content: {
+                googleMap: true,
+                image: false,
+                text: null
+            },
+            comments: [
+                {
+                    divider: true,
+                    id: 1,
+                    avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
+                    title: '<a>Jessica Nilson</a> 7 mins ago',
+                    subtitle: 'Someone said they were the best people out there just few years ago. Don\'t know better options.'
+                },
+                {
+                    divider: true,
+                    id: 2,
+                    avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
+                    title: '<a>Jessica Nilson</a> 7 mins ago',
+                    subtitle: 'Someone said they were the best people out there just few years ago. Don\'t know better options.'
+                },
+            ]
+        },
+        {
+            color: 'info',
+            icon: 'mdi-message',
+            opposite: 'yesterday<h5>8:03 pm</h5>',
+            title: 'Jessica Nilson @jess',
+            subtitle: '10:12 am - Publicly near Minsk',
+            content: {
+                googleMap: false,
+                image: false,
+                text: 'There is no such thing as maturity. There is instead an ever-evolving process of maturing. Because when there is a maturity, there is ...'
+            }
+        },
+        {
+            color: 'green lighten-1',
+            icon: 'mdi-coffee',
+            opposite: 'yesterday<h5>8:03 pm</h5>',
+            title: 'Jessica Nilson @jess',
+            subtitle: '10:12 am - Publicly near Minsk',
+            content: {
+                googleMap: false,
+                image: true,
+                text: 'Check out this awesome photo I made in Italy last summer. Seems it was lost somewhere deep inside my brand new HDD 40TB. Thanks god I found it!'
+            },
+            comments: [
+                {
+                    divider: true,
+                    id: 1,
+                    avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
+                    title: '<a>Jessica Nilson</a> 7 mins ago',
+                    subtitle: 'Someone said they were the best people out there just few years ago. Don\'t know better options.'
+                },
+            ]
+        },
+        {
+            color: 'indigo',
+            icon: 'mdi-alpha-j',
+            opposite: 'yesterday<h5>8:03 pm</h5>',
+            title: 'Jessica Nilson @jess',
+            subtitle: '10:12 am - Publicly near Minsk',
+            content: {
+                googleMap: false,
+                image: false,
+                text: 'Let\'s try something different this time. Hey, do you wanna join us tonight? We\'re planning to a launch a new project soon. Want to discuss with all of you...'
+            }
+        },
+    ]
+}
