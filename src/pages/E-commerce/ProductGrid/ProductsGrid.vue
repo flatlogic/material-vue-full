cc0d42089445d0f73bf0498bbe18abb5ac8c9d64
Blari <Blarior@gmail.com>
2020-08-23 00:48:24 +0300

    finish grid page, add assets

--- src/pages/E-commerce/ProductGrid/ProductsGrid.vue
+++ src/pages/E-commerce/ProductGrid/ProductsGrid.vue
@@ -1,40 +1,56 @@
 <template>
-  <v-container fluid class="gallery-page">
+  <v-container fluid class="product-grid-page">
     <v-row no-gutters class="d-flex mt-10 mb-6">
       <h1 class="page-title">Products Grid</h1>
     </v-row>
     <v-row>
-      <template v-for="card in cards">
+      <v-col
+          v-for="(select, i) in selects"
+          :key="i"
+          class="d-flex" cols="12" sm="6" md="4" lg="2">
+        <v-select
+          :key="i"
+          :items="select.items"
+          :label="select.type"
+          :value="select.items[0]"
+          outlined
+        ></v-select>
+      </v-col>
+    </v-row>
+    <v-row>
+      <template v-for="(card, i) in cards">
         <v-col
           cols="12"
           md="6"
           lg="3"
-          :key="card.id"
-        >
+          :key="i">
           <v-card
             class="mx-1 mb-1"
             link
             replace
-            :key="card.id"
-          >
+            :key="i">
             <v-img
               height="200"
               :src="card.src"
             ></v-img>
-
-            <v-card-text>
-              Visit ten places on our planet that are undergoing the biggest changes today.
-            </v-card-text>
-            <v-card-actions>
-              <v-btn icon>
-                <v-icon>mdi-heart</v-icon>
-              </v-btn>
-              <v-btn icon>
-                <v-icon>mdi-share-variant</v-icon>
-              </v-btn>
-              <v-btn icon>
-                <v-icon>mdi-comment</v-icon>
-              </v-btn>
+            <v-chip v-if="card.badge"
+              :color="card.badge.color"
+            >
+              {{ card.badge.content }}
+            </v-chip>
+            <v-card-title>
+              {{ card.title }}
+            </v-card-title>
+            <v-card-subtitle>
+              {{ card.subtitle }}
+            </v-card-subtitle>
+            <v-card-actions class="pa-4">
+              <span>{{ '$' + card.price }}</span>
+              <v-spacer></v-spacer>
+              <div class="d-flex align-center">
+                <span class="warning--text" style="font-size: 1rem">{{ card.rating }}&#160;&#160;</span>
+                <v-icon color="warning">mdi-star</v-icon>
+              </div>
             </v-card-actions>
           </v-card>
         </v-col>
@@ -51,55 +67,163 @@ export default {
       cards: [
         {
           id: 1,
-          src: require('@/assets/img/e-commerce/1.png')
+          title: 'Trainers',
+          subtitle: 'Trainers in white',
+          src: require('@/assets/img/e-commerce/medium/1.png'),
+          price: '78',
+          rating: '4.6',
+          badge: {
+            color: 'primary',
+            content: 'New'
+            }
         },
         {
           id: 2,
-          src: require('@/assets/img/e-commerce/2.png')
+          title: 'Boots',
+          subtitle: 'Trainers in blue',
+          src: require('@/assets/img/e-commerce/medium/2.png'),
+          price: '78',
+          rating: '4.6',
+          badge: {
+            color: 'secondary',
+            content: 'Sale'
+          }
         },
         {
           id: 3,
-          src: require('@/assets/img/e-commerce/3.png')
+          title: 'Flat sandals',
+          subtitle: 'Trainers in white',
+          src: require('@/assets/img/e-commerce/medium/3.png'),
+          price: '78',
+          rating: '4.6',
         },
         {
           id: 4,
-          src: require('@/assets/img/e-commerce/4.png')
+          title: 'Trainers',
+          subtitle: 'Trainers in blue',
+          src: require('@/assets/img/e-commerce/medium/4.png'),
+          price: '78',
+          rating: '4.6',
+          badge: {
+            color: 'primary',
+            content: 'New'
+          }
         },
         {
           id: 5,
-          src: require('@/assets/img/e-commerce/5.png')
+          title: 'Flat sandals',
+          subtitle: 'Trainers in white',
+          src: require('@/assets/img/e-commerce/medium/5.png'),
+          price: '78',
+          rating: '4.6',
+          badge: {
+            color: 'primary',
+            content: 'New'
+          }
         },
         {
           id: 6,
-          src: require('@/assets/img/e-commerce/6.png')
+          title: 'Flat sandals',
+          subtitle: 'Trainers in white',
+          src: require('@/assets/img/e-commerce/medium/6.png'),
+          price: '78',
+          rating: '4.6',
+          badge: {
+            color: 'secondary',
+            content: 'Sale'
+          }
         },
         {
           id: 7,
-          src: require('@/assets/img/e-commerce/1.png')
+          title: 'Trainers',
+          subtitle: 'Trainers in white',
+          src: require('@/assets/img/e-commerce/medium/1.png'),
+          price: '78',
+          rating: '4.6',
+          badge: {
+            color: 'primary',
+            content: 'New'
+          }
         },
         {
           id: 8,
-          src: require('@/assets/img/e-commerce/2.png')
+          title: 'Boots',
+          subtitle: 'Trainers in blue',
+          src: require('@/assets/img/e-commerce/medium/2.png'),
+          price: '78',
+          rating: '4.6',
         },
         {
           id: 9,
-          src: require('@/assets/img/e-commerce/3.png')
+          title: 'Flat sandals',
+          subtitle: 'Trainers in white',
+          src: require('@/assets/img/e-commerce/medium/3.png'),
+          price: '78',
+          rating: '4.6',
+          badge: {
+            color: 'primary',
+            content: 'New'
+          }
         },
         {
           id: 10,
-          src: require('@/assets/img/e-commerce/4.png')
+          title: 'Flat sandals',
+          subtitle: 'Trainers in white',
+          src: require('@/assets/img/e-commerce/medium/4.png'),
+          price: '78',
+          rating: '4.6',
         },
         {
           id: 11,
-          src: require('@/assets/img/e-commerce/5.png')
+          title: 'Boots',
+          subtitle: 'Trainers in blue',
+          src: require('@/assets/img/e-commerce/medium/5.png'),
+          price: '78',
+          rating: '4.6',
         },
         {
           id: 12,
-          src: require('@/assets/img/e-commerce/6.png')
-        },
-      ]
+          title: 'Boots',
+          subtitle: 'Trainers in blue',
+          src: require('@/assets/img/e-commerce/medium/6.png'),
+          price: '78',
+          rating: '4.6',
+          badge: {
+            color: 'secondary',
+            content: 'Sale'
+          }
+        }
+      ],
+      selects: [
+          {
+            type: 'Type',
+            items: ['Shoes', 'Boots', 'Trainers'],
+          },
+          {
+            type: 'Brands',
+            items: ['All', 'Nike', 'Adidas']
+          },
+          {
+            type: 'Size',
+            items: ['7', '8', '9', '10', '11']
+          },
+          {
+            type: 'Color',
+            items: ['All', 'White', 'Black']
+          },
+          {
+            type: 'Range',
+            items: ['All', '-', 'None']
+          },
+          {
+            type: 'Sort',
+            items: ['Favorite', 'Price', 'Popular']
+          },
+      ],
     }
   }
 }
 
 </script>
+
+<style src="./ProductGrid.scss" lang="scss"></style>
